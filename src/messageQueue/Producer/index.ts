b498b6720ce912eb3e4693ef7de2d1a0ccc65179
La <laduchai20111995@gmail.com>
2026-08-16 12:12:56 +0700

    customer consume

--- src/messageQueue/Producer/index.ts
+++ src/messageQueue/Producer/index.ts
@@ -1,5 +1,6 @@
 import { rabbit_server } from '@src/connect';
 import { MessageZaloField } from '../type';
+import { VideoMessageBodyField } from '../../dataStruct/message_v1/body';
 
 rabbit_server.init();
 
@@ -10,17 +11,23 @@ export async function sendMessage(queue: string, messageZalo: MessageZaloField) 
     channel.sendToQueue(queue, Buffer.from(JSON.stringify(messageZalo)), { persistent: true });
 }
 
-// export async function sendHookData(queue: string, hookData: any) {
-//     await rabbit_server.init();
-//     const channel = await rabbit_server.createChannel();
+export async function sendHookData(queue: string, hookData: any) {
+    const channel = await rabbit_server.createChannel();
 
-//     await channel.assertQueue(queue, { durable: true });
-//     channel.sendToQueue(queue, Buffer.from(JSON.stringify(hookData)), { persistent: true });
-// }
+    await channel.assertQueue(queue, { durable: true });
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(hookData)), { persistent: true });
+}
 
 export async function sendStringMessage(queue: string, msg: string) {
     const channel = await rabbit_server.createChannel();
 
     await channel.assertQueue(queue, { durable: true });
     channel.sendToQueue(queue, Buffer.from(msg), { persistent: true });
+}
+
+export async function sendVideoMessage(queue: string, videoMessageBody: VideoMessageBodyField) {
+    const channel = await rabbit_server.createChannel();
+
+    await channel.assertQueue(queue, { durable: true });
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(videoMessageBody)), { persistent: true });
 }
